bd9ad3c0b2e5b8080c2c91bb31065da58bf1c86c
Ian Frankel <ian.frankel@gmail.com>
2023-10-20 14:21:10 -0400

    pay the workers with context

--- index.js
+++ index.js
@@ -9,6 +9,8 @@
  for you to use if you need it!
  */
 
+let employeeRecord
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -21,3 +23,88 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(individualPersonalData) {
+    employeeRecord = {firstName: individualPersonalData[0],
+        familyName: individualPersonalData[1],
+        title: individualPersonalData[2],
+        payPerHour: individualPersonalData[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employeeRecord
+}
+
+function createEmployeeRecords(MultiplePersonalData) {
+    return MultiplePersonalData.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(dateStamp) {
+    let timeInEvent = {
+        type: 'TimeIn',
+        hour: getHour(dateStamp),
+        date: getDate(dateStamp)
+    }
+    this.timeInEvents.push(timeInEvent)
+    return this
+    
+}
+
+function createTimeOutEvent(dateStamp) {
+    const timeOutEvent = {
+        type: 'TimeOut',
+        hour: getHour(dateStamp),
+        date: getDate(dateStamp)
+    }
+    this.timeOutEvents.push(timeOutEvent)
+    return this
+}
+
+function getDate(dateStamp) {
+    //console.log(dateStamp)
+    return dateStamp.slice(0,10)
+}
+
+function getHour(dateStamp) {
+    //console.log(dateStamp)
+    return Number(dateStamp.slice(11,15))
+}
+
+function hoursWorkedOnDate(date) {
+    
+    let hoursWorked = 0
+    for (let index = 0; index < this.timeInEvents.length; index++) {
+        
+        if (this.timeOutEvents[index]['date']===date) {
+            hoursWorked = hoursWorked + this.timeOutEvents[index]['hour']
+        }
+    }
+    for (let index = 0; index < this.timeOutEvents.length; index++) {
+        if (this.timeInEvents[index]['date']===date) {
+            hoursWorked = hoursWorked - this.timeInEvents[index]['hour']
+        }        
+    }
+
+    return hoursWorked /100
+}
+
+function wagesEarnedOnDate(date) {
+    
+    let hoursCounter = hoursWorkedOnDate.bind(this)
+
+    return this.payPerHour * hoursCounter(date)
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    
+    for (let employee of srcArray) {
+        if (employee.firstName===firstName) {
+            return employee
+        }
+    }
+    
+    return undefined
+}
+
+function calculatePayroll(srcArray) {
+    return srcArray.map(employee => allWagesFor.bind(employee)()).reduce((a,b) => a + b, 0)
+}
